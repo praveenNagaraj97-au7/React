1a991a508e4d5cec4529f07128f562a70c2bc095
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-07-28 00:09:32 +0530

    Updated Nav Links

--- 06Hooks/src/components/Link.js
+++ 06Hooks/src/components/Link.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Link = ({ href, children }) => {
   const onClickLink = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
     window.history.pushState([], "", href);
 
