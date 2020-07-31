018142e15d953754102c60764255e3eaae490dac
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-08-01 00:01:02 +0530

    Change

--- 08redux-thunk-store-andmore/src/actions/index.js
+++ 08redux-thunk-store-andmore/src/actions/index.js
@@ -5,3 +5,9 @@ export const fetchPosts = () => async (dispatch) => {
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUsers = (id) => async (dispatch) => {
+  const response = await jsonPlaceHolderApi.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USERS", payload: response.data });
+};
